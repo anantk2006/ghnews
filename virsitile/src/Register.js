73d54b421d715107c6a6b66960f9c238cbcf098d
Anant Khandelwal <anantk2006@gmail.com>
2024-11-15 13:39:07 -0500

    post req written, need to make backend server. will deploy using ECS with a docker

--- virsitile/src/Register.js
+++ virsitile/src/Register.js
@@ -1,16 +1,37 @@
 // MyComponent.js
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import './index.css';
 
 function Register() {
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
-  const paramValue = queryParams.get('code'); // This will get 'sgdgdes'
+  const paramValue = queryParams.get('code');
+    React.useEffect(() => {
+        if (paramValue) {
+            fetch('http://localhost:3000/api/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ code: paramValue }),
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+        }
+    }, [paramValue]);
 
   return (
-    <div>
-      <h1>Query Parameter Value</h1>
-      <p>param = {paramValue}</p>
+    <div className="landing-page">
+    <header className="hero-section">
+        <h1>Connected!</h1>
+
+    </header>
     </div>
   );
 }
