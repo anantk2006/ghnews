fb1fa9878ff46500fb111b12d13e9153a4a67f61
Anant Khandelwal <anantk2006@gmail.com>
2025-01-02 22:55:55 -0500

    debugged

--- backend/newsletter.py
+++ backend/newsletter.py
@@ -17,14 +17,18 @@ def main():
     app = FirecrawlApp(api_key="fc-571037d21e434541b3747bfdecb42eae")
     llm = LLMWrapper()
     links = []
-    for topic in topics[:10]:
+    for topic in topics[:1]:
         relevant_info = find_relevant_info(topic, " news")
         dates_of_pub = ["9"*20 if 'datePublished' not in r else r['datePublished'] for r in relevant_info['webPages']['value']]
         recent = {i for i, d in enumerate(dates_of_pub) if d and int(d[2:4])>=24 and int(d[5:7])>=10}
         relevant_info = [i for i in relevant_info['webPages']['value'] if llm.classify_importance(i['name'])]
         links.extend([(topic, r['url']) for i, r in enumerate(relevant_info) if i in recent])
-    batch_scrape_result = app.batch_scrape_urls([l[1] for l in links][:5], {'formats': ['markdown']})
-    print(type(batch_scrape_result))
+    batch_scrape_result = app.batch_scrape_urls([l[1] for l in links][:1], {'formats': ['markdown']})
+    print(type(batch_scrape_result['data']))
+    if isinstance(batch_scrape_result['data'], list):
+        print(batch_scrape_result['data'][0].keys())
+    else:
+        print(batch_scrape_result['data'].keys())
     
     
 
