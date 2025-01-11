88610a18e6f7fa81b5c45eb6362c8a082b380e03
Anant Khandelwal <anantk2006@gmail.com>
2025-01-11 17:44:03 -0500

    modded database protocol for advacned scraping + link cache

--- backend/init.py
+++ backend/init.py
@@ -7,26 +7,50 @@ cursor = conn.cursor()
 cursor.execute('''DROP TABLE IF EXISTS users''')
 cursor.execute('''
 CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
     username TEXT,
-    user_email TEXT PRIMARY KEY,
+    user_email TEXT,
     access_token TEXT,
     signup_date TEXT
 )
 ''')
-
-# Commit the changes and close the connection
 cursor.execute('''DROP TABLE IF EXISTS topics''')
 cursor.execute('''
 CREATE TABLE topics (
-    username TEXT,
-    topic TEXT,
+    topic_id SERIAL PRIMARY KEY,
+    topic TEXT
+)
+''')
+def get_topics_from_file():
+    with open("topics.txt") as f:
+        topics = f.read().split("), ")
+        topics = [topic.split(" (") for topic in topics if len(topic) > 2]
+        types = [topic[1].split(", ") for topic in topics]
+        topics = [topic[0] for topic in topics]
+        return topics, types            
+
+topics, _ = get_topics_from_file()
+for topic in topics:
+    cursor.execute('''
+    INSERT INTO topics (topic)
+    VALUES (?)
+    ''', (topic,))
+
+# Commit the changes and close the connection
+cursor.execute('''DROP TABLE IF EXISTS user_skills''')
+cursor.execute('''
+CREATE TABLE user_skills (
+    user_id TEXT,
+    topic_id TEXT,
     skill_level FLOAT
 )
 ''')
+
 cursor.execute('''DROP TABLE IF EXISTS links''')
 cursor.execute('''
 CREATE TABLE links (
     topic TEXT,
+    search_type TEXT,
     link TEXT
 )
 ''')
