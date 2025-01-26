24fd4f4e583a7ced89a32ef8851d7b5e92b11144
Vidyoot Senthil <74870683+vid277@users.noreply.github.com>
2025-01-25 21:36:44 -0500

    feature: added pricing section

---
+++ frontend/src/app/sections/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <div className="h-10 bg-white flex items-center justify-center py-16">
+      <p className="text-sm font-hanken">
+        © 2025 Virsitile. All rights reserved.
+      </p>
+    </div>
+  );
+}
