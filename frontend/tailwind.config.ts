ae91e654838786a55efcc705f397616e0deadc8a
Vidyoot Senthil <74870683+vid277@users.noreply.github.com>
2025-01-25 20:59:46 -0500

    feature: hero div complete

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -57,7 +57,21 @@ export default {
         sm: "calc(var(--radius) - 4px)",
       },
       fontFamily: {
-        oddlini: ["var(--font-oddlini)"],
+        oddlini: ["Oddolini", "sans-serif"],
+        hanken: ["Hanken", "sans-serif"],
+      },
+    },
+    animation: {
+      "shiny-text": "shiny-text 8s infinite",
+    },
+    keyframes: {
+      "shiny-text": {
+        "0%, 90%, 100%": {
+          "background-position": "calc(-100% - var(--shiny-width)) 0",
+        },
+        "30%, 60%": {
+          "background-position": "calc(100% + var(--shiny-width)) 0",
+        },
       },
     },
   },
